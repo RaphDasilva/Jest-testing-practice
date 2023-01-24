1a82e4db59b025a411ea1a1a6c7c6ba962fe5769
Raphael <okolonnaemeka2017@gmail.com>
2023-01-24 11:49:18 -0500

    Add tests for the new functionality.

--- modules/string-length.test.js
+++ modules/string-length.test.js
@@ -3,3 +3,11 @@ const stringLength = require('./string-length.js');
 test('King is 4 latters', () => {
   expect(stringLength('king')).toBe(4);
 });
+
+test('The string length for encyclopedia is greater than 10', () => {
+  expect(stringLength('encyclopedia')).toBe('Conditions are not met');
+});
+
+test('string Length less than 1', () => {
+  expect(stringLength('')).toBe('Conditions are not met');
+});
